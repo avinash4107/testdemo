36956dd662be4d3903b545d3eab1598c9dd573a1
avinash4107 <48579794+avinash4107@users.noreply.github.com>
2019-05-15 15:44:34 +0530

    uploading a file

---
+++ Test1/src/TT.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+
+public class TT {
+
+	public static void main(String[] args) throws IOException {
+
+		
+		
+		File fil=new File(".");
+		System.out.println(fil.getCanonicalPath());
+		
+	}
+
+}
